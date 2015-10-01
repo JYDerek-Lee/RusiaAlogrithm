811b6dc383da80ea366685530686d1e9150e8f67
JuneYoungs <asplannedl@gmail.com>
2015-10-01 13:45:17 +0900

    코드 완성본

---
+++ RusiaAlogrithm/Al.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define INPUT1 19
+#define INPUT2 22
+
+int RusiaAlgorithm(int input1, int input2) {
+	int sum=0;
+	while (input1 > 0) {
+		sum += (input1 % 2 ? input2 : 0);
+		input1 /= 2;
+		input2 *= 2;
+	}
+	return sum;
+}
+
+void main() {
+	printf("%d\n", ((INPUT1 == 0 || INPUT2 == 0) ? exit(1) : RusiaAlgorithm(INPUT1, INPUT2)));
+}
